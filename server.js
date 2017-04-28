b12d107091664807768c9cd23200641069fc9ac9
flotos <flotos@hotmail.fr>
2017-04-28 14:21:35 +0100

    Testing another https

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var redirectToHTTPS = require('express-http-to-https')
+var toHttps = require('express-to-https').basic
 
+
+app.use(toHttps)
 app.use(express.static(__dirname + '/dist'));
-
-app.use(redirectToHTTPS(['localhost:8080']));
 
 app.get('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
